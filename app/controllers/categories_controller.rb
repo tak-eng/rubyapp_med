5627cd579a382f0bd8174ea902b4e3c61f885821
tak-eng <shibamoto2411@gmail.com>
2021-02-27 15:11:54 +0900

    カテゴリーコントローラーの記述

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,31 @@
 class CategoriesController < ApplicationController
+  def index
+    @category = Category.includes(:ideas).all.order('created_at DESC')
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(post_params)
+
+    if @post.save
+      flash[:notice] = "投稿が保存できました"
+      redirect_to root_path
+    else    
+      @post = Post.new
+      flash.now[:alert] = "投稿できません、もう一度入力してください"
+      render :new
+    end
+  end
+
+  private
+  def post_params
+    params.require(:category).permit(:name, :tag_list)
+  end
+
+end
+
+
 end
